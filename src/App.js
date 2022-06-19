0185bfe051c0c2fa48b0933d0e78d7b3d1a775d6
Arnold Kokot <ogkokot@gmail.com>
2022-06-19 08:30:44 +0200

    +source map loader

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
 import React from "react";
 
-export const App = () => <h1>Sample text</h1>;
+export const App = () => (
+  <>
+    <h1>Sample text</h1>
+  </>
+);
--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,11 @@ module.exports = {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
     ],
   },
   plugins: [
